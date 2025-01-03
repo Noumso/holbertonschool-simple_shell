f1dac210427fa5d9423ba16c9fceb9195ac3ff4d
hmeyd <yyahmedsalm@gmail.com>
2025-01-03 03:35:07 +0100

    j ai ajouter des fonction

--- shell.h
+++ shell.h
@@ -20,5 +20,16 @@ char **tknelize(char *buffer);
 char *chercher_commande(char *commande);
 char *verifier_commande(char **args);
 void executer_env(void);
+int _putchar(char c);
+char *_strncpy(char *dest, char *src, int n);
+int _strlen(char *s);
+int _atoi(char *s);
+void _puts(char *str);
+unsigned int check_delim(char c, const char *str);
+char *_strtok(char *str, const char *delim);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strdup(char *str);
 #endif
 
