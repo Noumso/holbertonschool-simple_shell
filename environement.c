330f983840d3c62bf57259857a4474eddbfb013e
hmeyd <yyahmedsalm@gmail.com>
2025-01-04 11:27:59 +0100

    environement

--- environement.c
+++ environement.c
@@ -10,7 +10,7 @@ void executer_env(void)
 	env = environ;
 	while (*env)
 	{
-		write(STDOUT_FILENO, *env, strlen(*env));
+		write(STDOUT_FILENO, *env, _strlen(*env));
 		write(STDOUT_FILENO, "\n", 1);
 		env++;
 	}
